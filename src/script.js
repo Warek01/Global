bce0807cf79ce7baae9393180aac616eded03f94
Warek <alexandrudobrojan@gmail.com>
2020-09-10 19:11:10 +0300

    Content added & fixes

--- src/script.js
+++ src/script.js
@@ -1,6 +1,8 @@
 "use strict";
-const menuButton = $("button#to-menu");
+const menu_button = $("button#to-menu");
 const content = $("#global-container");
+const side_menu = $("nav#side-menu");
+const side_map = $("#side-map-wrap");
 const slide_1 = content.find(".slide-1"),
       slide_2 = content.find(".slide-2"),
       slide_3 = content.find(".slide-3"),
@@ -10,34 +12,72 @@ const slide_1 = content.find(".slide-1"),
 
 /* Global container minimization logic */
 
-let minimized = false;
+let is_minimized = false;
 
-menuButton.click(e => {
+menu_button.click(minimize);
 
-   if (!minimized) {
+content.click(maximize);
+
+function maximize(e) {
+   if (is_minimized) {
+      content.removeClass("minimized");
+      side_menu.attr("id", "side-menu");
+      side_map.css({
+         "filter": "opacity(1)",
+         "pointer-events": "all"
+      });
+   }
+   is_minimized = false;
+}
+
+function minimize(e) {
+   if (!is_minimized) {
 
       content.addClass("minimized");
+      side_map.css({
+         "filter": "opacity(0)",
+         "pointer-events": "none"
+      });
 
       setTimeout(() => {
-         minimized = true;
+         side_menu.attr("id", "side-menu-active");
+      }, 100);
+
+      setTimeout(() => {
+         is_minimized = true;
       }, 500);
 
    }
-});
-
-content.click(function(e) {
-   if (minimized) 
-      $(this).removeClass("minimized");
-   minimized = false;
-});
+}
 
 
 /* -------------------------------------- */
 
+
+/* Side-Menu components */
+
+let active_slide = 1;
+
+side_menu.find("button").click(function(e) {
+   let calc = (active_slide - $(this).data("order")) * $("#slide-container").height() - 200 /* / 5 */;
+   if (active_slide !== $(this).data("order")) {
+      if (calc < 0) {
+         $("#slide-container").css("top", calc);
+      } else {
+         $("#slide-container").css("top", 0);
+      }
+      active_slide = $(this).data("order");
+   }
+   maximize();
+});
+
+/* -------------------------------------- */
+
+
  /* Slide-1 components */
 
 slide_1.find("section .title").click(function(e) {
-   if (!$(this).hasClass("active") && !minimized) {
+   if (!$(this).hasClass("active") && !is_minimized) {
       slide_1.find("section .active").removeClass("active");
       $(this).addClass("active");
       $(this).next().next().next().addClass("active");
